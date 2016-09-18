a3de3892c87001fb1bf9db05fac26dcc07ece348
Rafael Grilli <rafaelgrilli92@hotmail.com>
2016-09-18 18:33:20 +1000

    Finalize tests & add page title

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -53,6 +53,7 @@ var CountDown = React.createClass({
 
 		return (
 			<div>
+				<h1 className="page-title">Countdown App</h1>
 				<Clock totalSeconds={count}/>
 				{renderControlArea()}
 			</div>
--- app/tests/components/CountDown.test.jsx
+++ app/tests/components/CountDown.test.jsx
@@ -34,5 +34,29 @@ describe('CountDownComponent', () => {
 				done();
 			}, 3001)
 		});
+
+		it('Should pause countdown on paused status', (done) => {
+			var countdown = TestUtils.renderIntoDocument(<CountDownComponent />);
+			countdown.handleSetCountdown(3);
+			countdown.handleStatusChange('paused');
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(3);
+				expect(countdown.state.countdownStatus).toBe('paused');
+				done();
+			}, 1001)
+		});
+
+		it('Should stop countdown and put zero on stoped status', (done) => {
+			var countdown = TestUtils.renderIntoDocument(<CountDownComponent />);
+			countdown.handleSetCountdown(3);
+			countdown.handleStatusChange('stopped');
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(0);
+				expect(countdown.state.countdownStatus).toBe('stopped');
+				done();
+			}, 1001)
+		});
 	});
 });
